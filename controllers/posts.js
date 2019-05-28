ce3b08a43a36a23f6c83aab18ec6d7f108a94011
grus <gasperus@gmail.com>
2019-05-28 13:20:09 +0200

    Set map to show up on dynamic location for each of the post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,8 @@
+require('dotenv').config();
+
 const Post = require('../models/post');
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 const cloudinary = require('cloudinary');
 cloudinary.config({
   cloud_name: 'dfcxxwdrb',
@@ -34,6 +38,13 @@ module.exports = {
         public_id: image.public_id
       })
     }
+    let response = await geocodingClient
+      .forwardGeocode({
+        query: req.body.post.location,
+        limit: 1
+      })
+      .send();
+    req.body.post.coordinates = response.body.features[0].geometry.coordinates;
     //use req.body to create a new Post
     let post = await Post.create(req.body.post); //once we have a post we redirect post with id
     res.redirect(`/posts/${post.id}`); //this is show page
