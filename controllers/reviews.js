70f937e93ba661e5275b820d4d07bb18878c37b1
grus <gasperus@gmail.com>
2019-05-29 12:26:20 +0200

    Initial setup for reviews

---
+++ controllers/reviews.js
@@ -0,0 +1,19 @@
+const Post = require('../models/post');
+const Reviw = require('../models/review');
+
+
+module.exports = {
+   /* Reviews Create*/
+  async reviewCreate(req, res, next) {
+
+  },
+
+  //Reviews update
+  async reviewUpdate(req, res, next) {
+
+  },
+// Reviews Destroy
+  async reviwDestroy(req, res, next) {
+
+  }
+}
